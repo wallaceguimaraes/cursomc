5a28595b54929159f2600e6cfffe430819f249e8
Wallace <wallaceguimaraes7@outlook.com>
2022-01-08 23:50:23 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/wallaceguimaraes/cursomc/services/CategoriaService.java
+++ src/main/java/com/wallaceguimaraes/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.wallaceguimaraes.cursomc.domain.Categoria;
 import com.wallaceguimaraes.cursomc.repositories.CategoriaRepository;
+import com.wallaceguimaraes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	  public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-			return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
-	
+	 
+	 
 }

63980497b12b9502e7ce09145358d1b274972e87
Wallace <wallaceguimaraes7@outlook.com>
2022-01-10 00:18:55 -0300

    Estado e cidade

---
+++ src/main/java/com/wallaceguimaraes/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.wallaceguimaraes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wallaceguimaraes.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+	
+	
+}
---
+++ src/main/java/com/wallaceguimaraes/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,13 @@
+package com.wallaceguimaraes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wallaceguimaraes.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+	
+	
+}
